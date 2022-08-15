c2a85d0a15eecbe93be491644f20b482f8e8448e
aritra <aritrachowdhury65@gmail.com>
2022-08-15 09:27:57 +0530

    clear

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-var btntranslate = document.querySelector("#btn-translate")
-var txtInput = document.querySelector("#txt-input")
-var output = document.querySelector("#output")
-var clear = document.querySelector("#btn-clear")
+var btntranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var output = document.querySelector("#output");
+var clear = document.querySelector("#btn-clear");
 
-var url="https://api.funtranslations.com/translate/minion.json";
+var url = "https://api.funtranslations.com/translate/minion.json";
 
-function constructUrl(text){
-  return url+"?"+"text="+text;
+function constructUrl(text) {
+  return url + "?" + "text=" + text;
 }
 
 btntranslate.addEventListener("click", clickEventHandler);
 clear.addEventListener("click", clearEventHandler);
 
-function errorHandler(error){
-  output.innerText = "Error Occured "+ error
-  console.log("Error occured: "+error);
+function errorHandler(error) {
+  output.innerText = "Error Occured " + error;
+  console.log("Error occured: " + error);
 }
 
-function showOutput(text){
+function showOutput(text) {
   output.innerText = text;
 }
 
-function clearEventHandler(){
-  window.location.reload();
-  console.log("clear");
+function clearEventHandler() {
+  txtInput.value = "";
+  showOutput("");
 }
 function clickEventHandler() {
   var value = txtInput.value;
   fetch(constructUrl(value))
-  .then(response => response.json())
-  .then(json => showOutput(json.contents.translated))
-  .catch(errorHandler)
+    .then((response) => response.json())
+    .then((json) => showOutput(json.contents.translated))
+    .catch(errorHandler);
 }
